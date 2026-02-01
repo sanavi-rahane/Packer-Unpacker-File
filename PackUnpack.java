d38085807775345fd8687eabf77c27ec3ed62850
sanavi-rahane <sanavirahane2005@gmail.com>
2026-02-01 13:52:10 +0530

    PackerUnpacker

---
+++ PackUnpack.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Pack
+{
+    public
+        String FolderName = null;
+        String PackFileName = null;
+
+        File Folderobj = null;
+
+        Scanner sobj = null;
+
+        //--------------------------------------
+        System.out.println("Enter Directory Name");
+        FolderName = sobj.nextLine();
+        Folderobj = new File(FolderName);
+        
+        if(Foderobj.exist())
+        {
+
+        }
+        else
+        {
+
+        }
+
+}
+class UnPack
+{
+
+}
+public class PackUnpack
+{
+    public static void main(String A[])
+    {
+        System.out.println("")
+    }
+}
